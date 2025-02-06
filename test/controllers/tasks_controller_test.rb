665be648d141ff67fac669add01657bd99f09545
Joseph <tanshindnmt@gmail.com>
2025-02-06 20:24:30 +0900

    Initial commit: Simple Task Manager with Devise and Sidekiq

---
+++ test/controllers/tasks_controller_test.rb
@@ -0,0 +1,57 @@
+require "test_helper"
+
+include Devise::Test::IntegrationHelpers  # Add this at the top if it's not there
+
+setup do
+  @user = users(:one)  # Load a test user from fixtures
+  sign_in @user             # Sign in the user before each test
+  @task = tasks(:one)       # Load a test task from fixtures
+end
+
+
+class TasksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @task = tasks(:one)
+  end
+
+  test "should get index" do
+    get tasks_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_task_url
+    assert_response :success
+  end
+
+  test "should create task" do
+    assert_difference("Task.count") do
+      post tasks_url, params: { task: { completed: @task.completed, description: @task.description, due_date: @task.due_date, title: @task.title, user_id: @task.user_id } }
+    end
+
+    assert_redirected_to task_url(Task.last)
+  end
+
+  test "should show task" do
+    get task_url(@task)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_task_url(@task)
+    assert_response :success
+  end
+
+  test "should update task" do
+    patch task_url(@task), params: { task: { completed: @task.completed, description: @task.description, due_date: @task.due_date, title: @task.title, user_id: @task.user_id } }
+    assert_redirected_to task_url(@task)
+  end
+
+  test "should destroy task" do
+    assert_difference("Task.count", -1) do
+      delete task_url(@task)
+    end
+
+    assert_redirected_to tasks_url
+  end
+end
