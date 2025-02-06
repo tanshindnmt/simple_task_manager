0369bd13016bd976845c9d390c42f55b7b8297c4
Joseph <tanshindnmt@gmail.com>
2025-02-07 01:50:39 +0900

    Fix Bootstrap import and asset pipeline issues

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,7 @@
+//= link_tree ../images
+//= link application.js
+//= link_tree ../stylesheets
+//= link @popperjs--core.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
